567eb58bda0343b5983c01da49f65f21b0223b2b
Joshua Johanan <josh359@gmail.com>
2014-02-02 16:20:16 -0500

    made the publish function more explicit

--- publish.py
+++ publish.py
@@ -6,19 +6,20 @@ from boto.s3.key import Key
 os.system('grunt')
 
 
-def upload(file_name):
+def upload(file_name, bucket):
     k = Key(bucket)
     k.key = file_name
     k.set_contents_from_filename(file_name)
 
+#the AWS keys are in the env - check Trello for them
 conn = boto.connect_s3()
 
-#the AWS keys are in the env - check Trello for them
 bucket = conn.get_bucket('runbrowser')
 
 files = ['index.html', 'cache.manifest', 'favicon.ico', 'leaflet.css', 'leaflet.js', 'runBrowser.min.js',
          'runBrowser.js', 'runbrowser57.png', 'runbrowser72.png', 'runbrowser114.png', 'zepto.min.js',
          'img/glyphicons-halflings-white.png', 'css/bootstrap.min.css', 'css/bootstrap-responsive.min.css',
          'stamen.js']
-map(upload, files)
+
+[upload(file, bucket) for file in files]
 
